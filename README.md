8ab32aa610917cbc20cc33fd87ee2230bbceb333
Jean <jeanluca.fernandes@totvs.com.br>
2020-11-13 14:32:19 -0200

    ultimos ajustes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # poc-acesso-banco-progress
 Azure - Criar POC para gravar dados no Banco Progress
+
+# Objetivo
+    [x] Criar SpringBoot de acesso ao banco progress para gravar informações no ERP.
+    
+    
--- src/main/java/com/logistica/integracoes/crud/CrudOperationsService.java
+++ src/main/java/com/logistica/integracoes/crud/CrudOperationsService.java
@@ -1,13 +1,10 @@
 package com.logistica.integracoes.crud;
 
-import com.logistica.integracoes.exceptions.DetailErrorException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.sql.SQLException;
-import java.util.Date;
 
 @Service
 public class CrudOperationsService {
@@ -19,7 +16,6 @@ public class CrudOperationsService {
     public int performTransacationOperation(String query) {
         if (query == null) throw new AssertionError();
 
-        int executeUpdate = entityManager.createNativeQuery(query).executeUpdate();
-        return executeUpdate;
+        return entityManager.createNativeQuery(query).executeUpdate();
     }
 }
